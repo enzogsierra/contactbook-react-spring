495c7de56acb7f55279ab6801c89a9d7b9bae4e8
Enzo Sierra <enzogsierra@gmail.com>
2023-09-26 16:14:26 -0300

    cambio: interfaz mejorada (header, footer)

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,12 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 
 export default function Footer()
 {
     return (
-        <footer className="mt-5 p-4 text-bg-dark">
-            <div className="container text-center">
-                <p className="fs-4 fw-semibold">Hecho por enzogsierra</p>
+        <footer className="mt-5 pt-4 pb-5 bg-dark text-white">
+            <div className="container">
+                <div className="pt-4 border-top border-secondary">
+                    <div className="d-flex justify-content-between gap-4">
+                        <p className="mb-0 fs-5 fw-semibold">Aplicación web diseñada por @enzogsierra</p>
+
+                        <div className="d-flex justify-content-between gap-4">
+                            <Link to="mailto:enzogsierra@gmail.com" className="fs-3 text-white text-decoration-none"><i className="bi bi-envelope-fill" title="Enviar email"></i></Link>
+                            <Link to="https://github.com/enzogsierra" className="fs-3 text-white text-decoration-none"><i className="bi bi-github" title="Ver Github"></i></Link>
+                            <Link to="https://www.linkedin.com/in/enzogsierra/" className="fs-3 text-white text-decoration-none"><i className="bi bi-linkedin" title="Ver LinkedIn"></i></Link>
+                        </div>
+                    </div>
+                </div>
             </div>
         </footer>
     );
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 
 export default function Header()
@@ -6,9 +7,19 @@ export default function Header()
     return (
         <header className="mb-4 p-4 text-bg-dark">
             <div className="container">
-                <nav className="navbar navbar-expanded-md navbar-dark bg-dark">
-                    <a href="/" className='navbar-brand'>Inicio</a>
-                </nav>
+                <div className="d-flex align-items-center justify-content-between gap-4">
+                    <Link to="/" className="text-white text-decoration-none">
+                        <h1 className="d-flex align-items-center gap-3">
+                            <i className="bi bi-journal-bookmark"></i>
+                            Inicio
+                        </h1>
+                    </Link>
+
+                    <div className="d-flex align-items-center justify-content-between gap-4">
+                        <Link to="https://github.com/enzogsierra" className="btn btn-outline-light fw-semibold" title="Ir a Github"><i className="bi bi-github"></i> Github</Link>
+                        <Link to="https://github.com/enzogsierra/contactbook-react-springboot" className="btn btn-outline-light fw-semibold" title="Ver código fuente"><i className="bi bi-code-slash"></i> Código fuente</Link>
+                    </div>
+                </div>
             </div>
         </header>
     );
